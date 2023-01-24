c33d94e87689a0cc65a8124eac063ad6330a79bb
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-01-24 16:41:30 -0500

    create a start function and an easy level board

--- sudoku.js
+++ sudoku.js
@@ -40,6 +40,31 @@ for (n = 0; n < 9; n++) {
   }
 }
 
+const easy = [
+  [null, null, null, 2, 6, null, 7, null, 1],
+  [null, 6, 8, null, null, 7, null, null, 9],
+  [1, 9, null, null, null, 4, 5, null, null],
+  [8, 2, null, 1, null, null, null, 4, null],
+  [null, null, 4, 6, null, 2, 9, null, null],
+  [null, 5, null, null, null, 3, null, 2, 8],
+  [null, null, 9, 3, null, null, null, 7, 4],
+  [null, 4, null, null, 5, null, null, 3, 6],
+  [7, null, 3, null, 1, 8, null, null, null]
+]
+let boardArray
+
 const start = () => {
-  let boardArry = new Array(81).fill('')
+  // let boardArry = new Array(81).fill('')
+  boardArray = easy
+  for (n = 0; n < 9; n++) {
+    const row = document.getElementById(`row${n}`)
+    for (i = 0; i < 9; i++) {
+      const box = row.children[`${i}`]
+      box.value = boardArray[n][i]
+      if (box.value != false) {
+        box.disabled = true
+      }
+    }
+  }
 }
+start()
