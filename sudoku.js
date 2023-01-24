eeef7a18300fe5f0c847dd5830d173cfea20a876
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-01-24 10:51:44 -0500

    fixed 9*9 grid with 9 rows 9 column, differentiate subgrids from whole

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,22 @@
 const restart = document.getElementById('replay')
 const board = document.querySelector('section')
 
-class Box {
+class Row {
   constructor(name) {
     this.name = name
+  }
+  createRow() {
+    const r = document.createElement('tr')
+    r.classList.add('row')
+    r.setAttribute('id', `row${this.name}`)
+    board.append(r)
+  }
+}
+
+class Box {
+  constructor(name, row) {
+    this.name = name
+    this.row = row
   }
   createBox() {
     const b = document.createElement('input')
@@ -12,47 +25,21 @@ class Box {
     b.setAttribute('type', 'text')
     b.setAttribute('onkeypress', 'return /[1-9]/i.test(event.key)')
     b.setAttribute('maxlength', '1')
-    board.append(b)
+    this.row.append(b)
   }
 }
 
-for (i = 0; i < 81; i++) {
-  const box = new Box(`${i}`)
-  box.createBox()
-  const bb = document.getElementById(i)
-  if (
-    box.name === '0' ||
-    box.name === '9' ||
-    box.name === '18' ||
-    box.name === '27' ||
-    box.name === '36' ||
-    box.name === '45' ||
-    box.name === '54' ||
-    box.name === '63'
-  ) {
-    bb.style.borderLeft = '2px solid rgb(19, 6, 6)'
-    bb.style.borderRight = '2px solid rgb(19, 6, 6)'
-    bb.style.borderTop = '2px solid rgb(19, 6, 6)'
-  } else if (
-    box.name === '73' ||
-    box.name === '74' ||
-    box.name === '75' ||
-    box.name === '76' ||
-    box.name === '77' ||
-    box.name === '78' ||
-    box.name === '79' ||
-    box.name === '80'
-  ) {
-    bb.style.borderTop = '2px solid rgb(19, 6, 6)'
-    bb.style.borderRight = '2px solid rgb(19, 6, 6)'
-    bb.style.borderBottom = '2px solid rgb(19, 6, 6)'
-  } else if (box.name === '72') {
-    bb.style.borderLeft = '2px solid rgb(19, 6, 6)'
-    bb.style.borderRight = '2px solid rgb(19, 6, 6)'
-    bb.style.borderTop = '2px solid rgb(19, 6, 6)'
-    bb.style.borderBottom = '2px solid rgb(19, 6, 6)'
-  } else {
-    bb.style.borderTop = '2px solid rgb(19, 6, 6)'
-    bb.style.borderRight = '2px solid rgb(19, 6, 6)'
+for (n = 0; n < 9; n++) {
+  const row = new Row(`${n}`)
+  row.createRow()
+  const eachRow = document.getElementById(`row${n}`)
+  for (i = 0; i < 9; i++) {
+    const box = new Box(`${i}`, eachRow)
+    box.createBox()
+    const bb = document.getElementById(i)
   }
+}
+
+const start = () => {
+  let boardArry = new Array(81).fill('')
 }
