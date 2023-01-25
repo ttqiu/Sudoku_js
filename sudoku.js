fdb897eabb63c237da0474b1acb0935add02681f
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-01-25 00:44:10 -0500

    create a play function for boradArry to get value  when enter value in empty box

--- sudoku.js
+++ sudoku.js
@@ -1,5 +1,6 @@
 const restart = document.getElementById('replay')
 const board = document.querySelector('section')
+let boardArray = []
 
 class Row {
   constructor(name) {
@@ -29,42 +30,59 @@ class Box {
   }
 }
 
-for (n = 0; n < 9; n++) {
-  const row = new Row(`${n}`)
-  row.createRow()
-  const eachRow = document.getElementById(`row${n}`)
-  for (i = 0; i < 9; i++) {
-    const box = new Box(`${i}`, eachRow)
-    box.createBox()
-    const bb = document.getElementById(i)
+const grid = () => {
+  for (n = 0; n < 9; n++) {
+    const row = new Row(`${n}`)
+    row.createRow()
+    const eachRow = document.getElementById(`row${n}`)
+    for (i = 0; i < 9; i++) {
+      const box = new Box(`${i}`, eachRow)
+      box.createBox()
+      const bb = document.getElementById(i)
+    }
   }
 }
 
 const easy = [
-  [null, null, null, 2, 6, null, 7, null, 1],
-  [null, 6, 8, null, null, 7, null, null, 9],
-  [1, 9, null, null, null, 4, 5, null, null],
-  [8, 2, null, 1, null, null, null, 4, null],
-  [null, null, 4, 6, null, 2, 9, null, null],
-  [null, 5, null, null, null, 3, null, 2, 8],
-  [null, null, 9, 3, null, null, null, 7, 4],
-  [null, 4, null, null, 5, null, null, 3, 6],
-  [7, null, 3, null, 1, 8, null, null, null]
+  [0, 0, 0, 2, 6, 0, 7, 0, 1],
+  [0, 6, 8, 0, 0, 7, 0, 0, 9],
+  [1, 9, 0, 0, 0, 4, 5, 0, 0],
+  [8, 2, 0, 1, 0, 0, 0, 4, 0],
+  [0, 0, 4, 6, 0, 2, 9, 0, 0],
+  [0, 5, 0, 0, 0, 3, 0, 2, 8],
+  [0, 0, 9, 3, 0, 0, 0, 7, 4],
+  [0, 4, 0, 0, 5, 0, 0, 3, 6],
+  [7, 0, 3, 0, 1, 8, 0, 0, 0]
 ]
-let boardArray
 
 const start = () => {
   // let boardArry = new Array(81).fill('')
   boardArray = easy
+  grid()
   for (n = 0; n < 9; n++) {
     const row = document.getElementById(`row${n}`)
     for (i = 0; i < 9; i++) {
       const box = row.children[`${i}`]
-      box.value = boardArray[n][i]
-      if (box.value != false) {
+      if (boardArray[n][i] !== 0) {
+        box.value = boardArray[n][i]
+        // box.innerText = parseInt(box.value)
         box.disabled = true
       }
     }
   }
 }
 start()
+
+const play = () => {
+  for (let n = 0; n < 9; n++) {
+    const row = document.getElementById(`row${n}`)
+    for (let i = 0; i < 9; i++) {
+      const box = row.children[`${i}`]
+      box.addEventListener('keypress', function () {
+        boardArray[n][i] = parseInt(box.value)
+        console.log(boardArray)
+      })
+    }
+  }
+}
+play()
